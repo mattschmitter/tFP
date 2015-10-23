ed1a49ef51c9c7092851e968ab6863ca76e996b6
Matt Schmitter <matt.schmitter@gmail.com>
2015-10-23 14:21:04 +0000

    Added unique tag to quote

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "unique_tag" do
+  	quote = Quote.create(:author => 'Scaredy Kat', :saying => "You lost your voice screaming")
+  	expected = "SK#" + quote.id.to_s
+  	actual = quote.unique_tag
+  	assert_equal expected, actual
   end
 end
