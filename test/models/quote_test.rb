72a172e5242c24a6a3d73bd5751ff50a960def87
Matt Schmitter <matt.schmitter@gmail.com>
2015-10-23 18:55:50 +0000

    Added Factory Girl

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
   test "unique_tag" do
-  	quote = Quote.create(:author => 'Scaredy Kat', :saying => "You lost your voice screaming")
+  	quote = FactoryGirl.create(:quote, :author => 'Scaredy Kat')
   	expected = "SK#" + quote.id.to_s
   	actual = quote.unique_tag
   	assert_equal expected, actual
